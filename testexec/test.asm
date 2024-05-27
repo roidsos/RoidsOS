57c02a153deda79bca818599c1609dd92701c7bf
lolguy91 <retek1544@gmail.com>
2024-05-27 15:00:45 +0000

    test bin prints "ass" to screen

--- testexec/test.asm
+++ testexec/test.asm
@@ -1,5 +1,20 @@
 global _start
 _start:
+    mov rax, 1
+    mov rdi, 'A'
+    mov rsi, 0
+    mov rdx, 0
+    int 0x80
+    mov rax, 1
+    mov rdi, 's'
+    mov rsi, 0
+    mov rdx, 0
+    int 0x80
+    mov rax, 1
+    mov rdi, 's'
+    mov rsi, 0
+    mov rdx, 0
+    int 0x80
     mov rax, 0
     mov rdi, 0
     mov rsi, 0
