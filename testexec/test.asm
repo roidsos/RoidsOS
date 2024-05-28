d02b59e889d0a6029999c7d8cc3ff8bd261db8bb
lolguy91 <retek1544@gmail.com>
2024-05-28 17:22:24 +0000

    longer message

--- testexec/test.asm
+++ testexec/test.asm
@@ -1,17 +1,62 @@
 global _start
 _start:
     mov rax, 1
-    mov rdi, 'A'
+    mov rdi, 'H'
     mov rsi, 0
     mov rdx, 0
     int 0x80
     mov rax, 1
-    mov rdi, 's'
+    mov rdi, 'e'
     mov rsi, 0
     mov rdx, 0
     int 0x80
     mov rax, 1
-    mov rdi, 's'
+    mov rdi, 'l'
+    mov rsi, 0
+    mov rdx, 0
+    int 0x80
+    mov rax, 1
+    mov rdi, 'l'
+    mov rsi, 0
+    mov rdx, 0
+    int 0x80
+    mov rax, 1
+    mov rdi, 'o'
+    mov rsi, 0
+    mov rdx, 0
+    int 0x80
+    mov rax, 1
+    mov rdi, ' '
+    mov rsi, 0
+    mov rdx, 0
+    int 0x80
+    mov rax, 1
+    mov rdi, 'W'
+    mov rsi, 0
+    mov rdx, 0
+    int 0x80
+    mov rax, 1
+    mov rdi, 'o'
+    mov rsi, 0
+    mov rdx, 0
+    int 0x80
+    mov rax, 1
+    mov rdi, 'r'
+    mov rsi, 0
+    mov rdx, 0
+    int 0x80
+    mov rax, 1
+    mov rdi, 'l'
+    mov rsi, 0
+    mov rdx, 0
+    int 0x80
+    mov rax, 1
+    mov rdi, 'd'
+    mov rsi, 0
+    mov rdx, 0
+    int 0x80
+    mov rax, 1
+    mov rdi, '!'
     mov rsi, 0
     mov rdx, 0
     int 0x80
